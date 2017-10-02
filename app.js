82fad268a091481efe782cb935a5d8ebcb16374f
rummzink <rummzink@users.noreply.github.com>
2017-10-02 12:38:50 +0700

    wdqdqd

--- app.js
+++ app.js
@@ -27,6 +27,81 @@ var bot = new builder.UniversalBot(connector, function (session) {
 var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
 bot.recognizer(recognizer);
 
+bot.dialog('CreateGameDialog', [
+    function (session) {
+        // Initialize game structure.
+        // - dialogData gives us temporary storage of this data in between
+        //   turns with the user.
+        var game = session.dialogData.game = { 
+            type: 'custom', 
+            sides: null, 
+            count: null,
+            turns: 0
+        };
+
+        /**
+         * Ask for the number of sides.
+         * 
+         * You can pass an array of choices to be matched. These will be shown as a
+         * numbered list by default.  This list will be shown as a numbered list which
+         * is what we want since we have so many options.
+         * 
+         * - value is what you want returned via 'results.response.entity' when selected.
+         * - action lets you customize the displayed label and for buttons what get sent when clicked.
+         * - synonyms let you specify alternate things to recognize for a choice.
+         */
+        var choices = [
+            { value: '4', action: { title: '4 Sides' }, synonyms: 'four|for|4 sided|4 sides' },
+            { value: '6', action: { title: '6 Sides' }, synonyms: 'six|sex|6 sided|6 sides' },
+            { value: '8', action: { title: '8 Sides' }, synonyms: 'eight|8 sided|8 sides' },
+            { value: '10', action: { title: '10 Sides' }, synonyms: 'ten|10 sided|10 sides' },
+            { value: '12', action: { title: '12 Sides' }, synonyms: 'twelve|12 sided|12 sides' },
+            { value: '20', action: { title: '20 Sides' }, synonyms: 'twenty|20 sided|20 sides' }
+        ];
+        // builder.Prompts.choice(session, 'choose_sides', choices, { 
+        //     speak: speak(session, 'choose_sides_ssml') 
+        // });
+        next({ response: 0 });
+    },
+    function (session, results) {
+        // Store users input
+        // - The response comes back as a find result with index & entity value matched.
+        var game = session.dialogData.game;
+        game.sides = Number(results.response.entity);
+
+        /**
+         * Ask for number of dice.
+         * 
+         * - We can use gettext() to format a string using a template stored in our
+         *   localized prompts file.
+         * - The number prompt lets us pass additional options to say we only want
+         *   integers back and what's the min & max value that's allowed.
+         */
+        var prompt = session.gettext('choose_count', game.sides);
+        builder.Prompts.number(session, prompt, {
+            speak: speak(session, 'choose_count_ssml'),
+            minValue: 1,
+            maxValue: 100,
+            integerOnly: true
+        });
+    },
+    function (session, results) {
+        // Store users input
+        // - The response is already a number.
+        var game = session.dialogData.game;
+        game.count = results.response;
+
+        /**
+         * Play the game we just created.
+         * 
+         * We can use replaceDialog() to end the current dialog and start a new
+         * one in its place. We can pass arguments to dialogs so we'll pass the
+         * 'PlayGameDialog' the game we created.
+         */
+        session.replaceDialog('PlayGameDialog', { game: game });
+    }
+]).triggerAction({ matches: 'CreateGameDialog'});
+
 bot.dialog('SearchHotels', [
     function (session, args, next) {
         session.send('Welcome to the Hotels finder! We are analyzing your message: \'%s\'', session.message.text);
